22581602d6d2b1a740d0c7a59a34e09dac82670f
MD:HASIBUL ISLAM SHAON <102863709+hasibulislamshaon@users.noreply.github.com>
2022-09-22 22:28:33 +0600

    Update one pad.py

--- one pad.py
+++ one pad.py
@@ -1,8 +1,22 @@
-def encrypt(pad, secret):
+"""def encrypt(pad, secret):
     return ''.join([chr(ord(c) ^ ord(secret[i])) for i, c in enumerate(pad[:len(secret)])])
+
+
+
+
+
 secret = 'i am a secret'
 pad = '33116f14-9b6f-42c6-9636-3dbd31c0548d'
 enc = encrypt(pad, secret)
-print(enc)
+print("Encrypted: ",enc)
 assert secret == encrypt(pad, enc)
-print(secret)
+print("Decrypted: ",secret)"""
+import onetimepad
+
+cipher = onetimepad.encrypt('One Time Cipher', 'random')
+print("Cipher text is ")
+print(cipher)
+print("Plain text is ")
+msg = onetimepad.decrypt(cipher, 'random')
+
+print(msg)
